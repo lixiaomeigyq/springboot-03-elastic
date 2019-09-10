2fce6f3c9d1c79c628a6b5146f971f93e54a20f3
lixiaomei <lixiaomei@lvmama.com>
2019-09-10 13:45:01 +0800

    dddd

---
+++ src/main/java/com/aiguigu/springboot/elastic/Springboot03ElasticApplication.java
@@ -0,0 +1,13 @@
+package com.aiguigu.springboot.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03ElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03ElasticApplication.class, args);
+	}
+
+}
